5ca24be77a78b9764d7ceefb3ca55c174f333b77
Ladislas WALCAK <walcak.ladislas@gmail.com>
2019-03-12 11:01:13 +0100

    Created MainActivity

---
+++ app/src/main/java/com/example/frigolo/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.frigolo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.main_activity); }}
