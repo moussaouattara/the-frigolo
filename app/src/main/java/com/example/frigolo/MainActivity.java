7323139fdb8b3e0c98f0a8143cbccf8454e6df9e
Michael Abid <abid@info27-24.iut45.univ-orleans.fr>
2019-03-13 14:27:22 +0100

    remise des fichier de base

--- app/src/main/java/com/example/frigolo/MainActivity.java
+++ app/src/main/java/com/example/frigolo/MainActivity.java
@@ -1,11 +1,50 @@
 package com.example.frigolo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_scan:
+                    mTextMessage.setText(R.string.title_scan);
+                    return true;
+                case R.id.navigation_myfridge:
+                    mTextMessage.setText(R.string.title_myfridge);
+                    return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText(R.string.title_setting);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
-    public void onCreate(Bundle savedInstance) {
-        super.onCreate(savedInstance);
-        setContentView(R.layout.main_activity); }}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        MenuItem i=(MenuItem) findViewById(R.id.navigation);
+
+
+
+    }
+
+
+}
