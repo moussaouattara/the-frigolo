5da385831bcbaaa3a39c4875731f8d1b8eeaf150
Michael Abid <abid@info21-20.iut45.univ-orleans.fr>
2019-03-27 15:24:19 +0100

    ajout des fragement

--- app/src/main/java/com/example/frigolo/MainActivity.java
+++ app/src/main/java/com/example/frigolo/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.frigolo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+//    private TextView mTextMessage;
+    private static FragmentHome fHome =new FragmentHome();
+    private static FragmentSetting fSetting =new FragmentSetting();
+    private static FragmentFridge fFridge =new FragmentFridge();
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +27,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_scan:
-                    mTextMessage.setText(R.string.title_scan);
+//                    mTextMessage.setText(R.string.title_scan);
+                    getFragmentManager().beginTransaction().replace(R.id.fragment, fHome).commit();
                     return true;
                 case R.id.navigation_myfridge:
-                    mTextMessage.setText(R.string.title_myfridge);
+//                    mTextMessage.setText(R.string.title_myfridge);
+                    getFragmentManager().beginTransaction().replace(R.id.fragment, fFridge).commit();
                     return true;
                 case R.id.navigation_setting:
-                    mTextMessage.setText(R.string.title_setting);
+//                    mTextMessage.setText(R.string.title_setting);
+                    getFragmentManager().beginTransaction().replace(R.id.fragment, fSetting).commit();
                     return true;
             }
             return false;
@@ -36,11 +48,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         MenuItem i=(MenuItem) findViewById(R.id.navigation);
+        getFragmentManager().beginTransaction().replace(R.id.fragment, fHome).commit();
+
 
 
 
